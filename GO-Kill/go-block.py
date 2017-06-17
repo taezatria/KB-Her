a8e2de9f6b40869413910c79d2cc475d10280728
taezatria <satria_the13@yahoo.co.id>
2017-06-17 13:30:29 +0700

    no file

--- GO-Kill/go-block.py
+++ GO-Kill/go-block.py
@@ -13,9 +13,10 @@ N = 9
 W = N + 2
 empty = "\n".join([(N+1)*' '] + N*[' '+N*'.'] + [(N+2)*' '])
 colstr = 'ABCDEFGHJKLMNOPQRST'
+strrand = 'ABCDEFGHI'
 MAX_GAME_LEN = N * N * 3
 
-N_SIMS = 500
+N_SIMS = 1400
 RAVE_EQUIV = 3500
 EXPAND_VISITS = 8
 PRIOR_EVEN = 10
@@ -52,7 +53,7 @@ pat3src = [
        ["XO?",
         "X..",
         "x.?"],
-       ["XOO",  
+       ["XOO",
         "...",
         "?.?"],
        [".O.",
@@ -197,13 +198,13 @@ class Position(namedtuple('Position', 'board cap n ko last last2 komi')):
             fboard = floodfill(board, d)
             if contact(fboard, '.') is not None:
                 continue  # some liberties left
-            
+
             capcount = fboard.count('#')
             if capcount == 1:
                 singlecaps.append(d)
             capX += capcount
             board = fboard.replace('#', '.')
-      
+
         ko = singlecaps[0] if in_enemy_eye and len(singlecaps) == 1 else None
 
         if contact(floodfill(board, c), '.') is None:
@@ -227,13 +228,13 @@ class Position(namedtuple('Position', 'board cap n ko last last2 komi')):
             elif i == -1:
                 i = 0
                 passes += 1
-                continue 
+                continue
             if is_eye(self.board, i) == 'X':
                 continue
             yield i
 
     def last_moves_neighbors(self):
-       
+
         clist = []
         for c in self.last, self.last2:
             if c is None:  continue
@@ -243,7 +244,7 @@ class Position(namedtuple('Position', 'board cap n ko last last2 komi')):
         return clist
 
     def score(self, owner_map=None):
-        
+
         board = self.board
         i = 0
         while True:
@@ -251,7 +252,7 @@ class Position(namedtuple('Position', 'board cap n ko last last2 komi')):
             if i == -1:
                 break
             fboard = floodfill(board, i)
-           
+
             touches_X = contact(fboard, 'X') is not None
             touches_x = contact(fboard, 'x') is not None
             if touches_X and not touches_x:
@@ -260,7 +261,7 @@ class Position(namedtuple('Position', 'board cap n ko last last2 komi')):
                 board = fboard.replace('#', 'x')
             else:
                 board = fboard.replace('#', ':')
-            
+
         komi = self.komi if self.n % 2 == 1 else -self.komi
         if owner_map is not None:
             for c in range(W*W):
@@ -274,14 +275,14 @@ def empty_position():
 
 
 def fix_atari(pos, c, singlept_ok=False, twolib_test=True, twolib_edgeonly=False):
-    
+
     def read_ladder_attack(pos, c, l1, l2):
 
         for l in [l1, l2]:
             pos_l = pos.move(l)
             if pos_l is None:
                 continue
-            
+
             is_atari, atari_escape = fix_atari(pos_l, c, twolib_test=False)
             if is_atari and not atari_escape:
                 return l
@@ -409,7 +410,7 @@ def load_spat_patterndict(f):
 
 large_patterns = dict()
 def load_large_patterns(f):
-   
+
     for line in f:
         p = float(line.split()[0])
         m = re.search('s:(\d+)', line)
@@ -542,7 +543,7 @@ class TreeNode():
         cfg_map = cfg_distances(self.pos.board, self.pos.last) if self.pos.last is not None else None
         self.children = []
         childset = dict()
- 
+
         for c, kind in gen_playout_moves(self.pos, range(N, (N+1)*W), expensive_ok=True):
             pos2 = self.pos.move(c)
             if pos2 is None:
@@ -797,39 +798,41 @@ def str_coord(c):
     row, col = divmod(c - (W+1), W)
     return '%c%d' % (colstr[col], N - row)
 
-
+HIST = []
 def goblock(arg):
-#	try:
-#		with open(spat_patterndict_file) as f:
-#			print('Loading pattern spatial dictionary...', file=sys.stderr)
-#			file=sys.stderr
-#			load_spat_patterndict(f)
-#		with open(large_patterns_file) as f:
-#			print('Loading large patterns...', file=sys.stderr)
-#			file=sys.stderr
-#			load_large_patterns(f)
-#		print('Done.', file=sys.stderr)
-#	except IOError as e:
-#        print('Warning: Cannot load pattern files: %s; will be much weaker, consider lowering EXPAND_VISITS 5->2' % (e,), file=sys.stderr)
-#		pass
+    tree = TreeNode(Position(board=arg, cap=(0, 0), n=0, ko=None, last=None, last2=None, komi=7.5))
+    tree.expand()
+    owner_map = W*W*[0]
+    tree = tree_search(tree, N_SIMS, owner_map)
+    ret = str_coord(tree.pos.last)
+    HIST.append(ret)
+    print('%s ' % ret)
+    return ret
 
-	tree = TreeNode(Position(board=arg, cap=(0, 0), n=0, ko=None, last=None, last2=None, komi=7.5))
-	tree.expand()
-	owner_map = W*W*[0]
-	tree = tree_search(tree, N_SIMS, owner_map)
-	ret = str_coord(tree.pos.last)
-	print('%s ' % ret)
-	return ret
+def ngasal():
+    while True:
+        test = True
+        out = random.choice(strrand) + str(random.randint(1, 9))
+        for i in HIST:
+            if out == i:
+                test = False
+                break
+        if (test):
+            HIST.append(out)
+            return out
 
 if __name__ == '__main__':
-	if(len(sys.argv) >= 2):
-		board = sys.argv[1]
-		goblock(board)
-	else:
-		board = empty_position().board
-		t_start = time.time()
-		goblock(board)
-		print('Tree search with %d playouts took %.3fs with %d threads; speed is %.3f playouts/thread/s' %
+    if(len(sys.argv) >= 2):
+        board = sys.argv[1]
+        try:
+            goblock(board)
+        except IndexError :
+            out = ngasal()
+            print('%s' % out)
+    else:
+        board = empty_position().board
+        t_start = time.time()
+        goblock(board)
+        print('Tree search with %d playouts took %.3fs with %d threads; speed is %.3f playouts/thread/s' %
               (N_SIMS, time.time() - t_start, multiprocessing.cpu_count(),
                N_SIMS / ((time.time() - t_start) * multiprocessing.cpu_count())))
-	
